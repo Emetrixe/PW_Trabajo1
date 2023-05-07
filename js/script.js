54dda9f84dbde1eb2b1a73f0fd99c6e8da914ab7
Eduardo <new60600@gmail.com>
2023-05-07 16:35:37 -0500

    modificacion p4

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-var splide = new Splide('.splide');
-var bar = splide.root.querySelector('.my-carousel-progress-bar');
+var myHeader = document.getElementsByClassName("myHeader");
 
-// Updates the bar width whenever the carousel moves:
-splide.on('mounted move', function () {
-  var end = splide.Components.Controller.getEnd() + 1;
-  var rate = Math.min((splide.index + 1) / end, 1);
-  bar.style.width = String(100 * rate) + '%';
+var scrollPosition = window.pageYOffset;
+
+window.addEventListener("scrolled", function() {
+  if (scrollPosition > 0) {
+    myHeader.classList.add("scrolled")
+  }
 });
-
-splide.mount();
